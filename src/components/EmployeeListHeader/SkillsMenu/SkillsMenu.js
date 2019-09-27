a4713d4bcaa7875be853ca86dc2710b87eb58fd8
chof09 <filip.piskur@gmail.com>
2019-09-27 20:07:42 +0200

    Skills menu now toggleable

---
+++ src/components/EmployeeListHeader/SkillsMenu/SkillsMenu.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styles from './SkillsMenu.module.css';
+
+import SkillCheckbox from './SkillCheckbox/SkillCheckbox';
+import Spinner from '../../UI/Spinner';
+
+function SkillsMenu (props) {
+
+    let musicalSkills = null, actingSkills = null, dancingSkills = null;
+    let menuContent = <Spinner />;
+
+    if (props.skills) {
+        musicalSkills = props.skills.musical.map(skill => (
+            <li key={ skill }>
+                <SkillCheckbox skill={ skill } />
+            </li>
+        ));
+        actingSkills = props.skills.acting.map(skill => (
+            <li key={ skill }>
+                <SkillCheckbox skill={ skill } />
+            </li>
+        ));
+        dancingSkills = props.skills.dancing.map(skill => (
+            <li key={ skill }>
+                <SkillCheckbox skill={ skill } />
+            </li>
+        ));
+
+        menuContent = (
+            <React.Fragment>
+                <button>Clear all</button>
+                <div className={ styles.skillsListWrapper }>
+                    <div>
+                        <h3>Musical</h3>
+                        <ul className={ styles.skillsList }>
+                            { musicalSkills }
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>Acting</h3>
+                        <ul className={ styles.skillsList }>
+                            { actingSkills }
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>Dancing</h3>
+                        <ul className={ styles.skillsList }>
+                            { dancingSkills }
+                        </ul>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
+
+    return (
+        <div className={ styles.SkillsMenu }>
+            { menuContent }
+        </div>
+    );
+}
+
+export default SkillsMenu;
--- src/containers/EmployeeListHeader/EmployeeListHeader.js
+++ src/containers/EmployeeListHeader/EmployeeListHeader.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styles from './EmployeeListHeader.module.css';
 import axios from '../../axios';
 
-import SkillCheckbox from '../../components/EmployeeListHeader/SkillCheckbox/SkillCheckbox';
+import SkillsMenu from '../../components/EmployeeListHeader/SkillsMenu/SkillsMenu';
 
 class EmployeeListHeader extends Component {
 
@@ -10,6 +10,7 @@ class EmployeeListHeader extends Component {
         skillsList: null,
         skillsFilter: [],
         availabilityFilter: "All",
+        menuVisible: false,
         error: false
     }
 
@@ -23,27 +24,11 @@ class EmployeeListHeader extends Component {
             });
     }
 
+    toogleMenu = () => {
+        this.setState((prevState) => ({ menuVisible: !prevState.menuVisible }));
+    }
+
     render () {
-
-        let musicalSkills = null, actingSkills = null, dancingSkills = null;
-
-        if (this.state.skillsList) {
-            musicalSkills = this.state.skillsList.musical.map(skill => (
-                <li key={ skill }>
-                    <SkillCheckbox skill={ skill } />
-                </li>
-            ));
-            actingSkills = this.state.skillsList.acting.map(skill => (
-                <li key={ skill }>
-                    <SkillCheckbox skill={ skill } />
-                </li>
-            ));
-            dancingSkills = this.state.skillsList.dancing.map(skill => (
-                <li key={ skill }>
-                    <SkillCheckbox skill={ skill } />
-                </li>
-            ));
-        }
 
         return (
             <div className={ styles.EmployeeListHeader }>
@@ -61,33 +46,17 @@ class EmployeeListHeader extends Component {
                     <button>Not Available</button>
                 </div>
                 <div>
-                    <button className={ styles.showSkillsBtn }>
+                    <button onClick={ this.toogleMenu } className={ styles.showSkillsBtn }>
                         <span>Chosen skills <small>(waltz, drama, comedy, action, horror, swing, triangle, contemporary)</small></span>
-                        <span className={ styles.arrow }>&#9660;</span>
+                        {
+                        this.state.menuVisible ?
+                            <span className={ styles.arrow }>&#9650;</span> : // up arrow
+                            <span className={ styles.arrow }>&#9660;</span>   // down arrow
+                        }
                     </button>
-                    <div className={ styles.skillsMenu }>
-                        <button>Clear all</button>
-                        <div className={ styles.skillsListWrapper }>
-                            <div>
-                                <h3>Musical</h3>
-                                <ul className={ styles.skillsList }>
-                                    { musicalSkills }
-                                </ul>
-                            </div>
-                            <div>
-                                <h3>Acting</h3>
-                                <ul className={ styles.skillsList }>
-                                    { actingSkills }
-                                </ul>
-                            </div>
-                            <div>
-                                <h3>Dancing</h3>
-                                <ul className={ styles.skillsList }>
-                                    { dancingSkills }
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+                    {
+                    this.state.menuVisible ? <SkillsMenu skills={ this.state.skillsList } /> : null
+                    }
                 </div>
             </div>
         );
