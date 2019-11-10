8c2c7773fbcd29561eb8d93f906022958cbabd80
chof09 <filip.piskur@gmail.com>
2019-11-10 22:45:24 +0100

    minor progress on ProfilePage component design

--- src/containers/ProfilePage/ProfilePage.js
+++ src/containers/ProfilePage/ProfilePage.js
@@ -3,6 +3,8 @@ import styles from './ProfilePage.module.css';
 
 import headerImage from '../../images/profile-header-image.png';
 import profileImage from '../../images/fa-user.png';
+import specialImage from '../../images/special-image.png';
+import tvShowImage from '../../images/tv-header-image.png';
 import TopBar from '../../components/UI/TopBar/TopBar';
 
 class ProfilePage extends Component {
@@ -12,7 +14,7 @@ class ProfilePage extends Component {
         return (
             <React.Fragment>
                 <TopBar />
-                <header className={ styles.ProfilePage }>
+                <header>
                     <div className={ styles.images }>
                         <div className={ styles.headerImage }>
                             <img src={ headerImage } alt="Header" />
@@ -34,11 +36,51 @@ class ProfilePage extends Component {
                             <p className={ styles.short }>Parturient metus rhoncus ullamcorper eu rhoncus erat proin placerat sem elit volutpat bibendum vivamus dui a in curabitur scelerisque a scelerisque tincidunt lobortis.</p>
                         </div>
                         <div className={ styles.bioFooter }>
-                            <a href="#">www.mywebsite.com</a>
+                            <a href="#" target="_blank">www.mywebsite.com</a>
                             <button>edit</button>
                         </div>
                     </div>
                 </header>
+                <main>
+                    <div className={ styles.navigation }>
+                        <button>Projects</button>
+                        <button>About me</button>
+                    </div>
+                    <section className={ styles.projects }>
+                        <article>
+                            <div className={ styles.articleHeaderImg }>
+                                <img src={ specialImage } alt="Project header" />
+                            </div>
+                            <div className={ styles.articleBody }>
+                                <div className={ styles.inline }>
+                                    <h3 className={ styles.articleHeading }>Sticks and Stones</h3>
+                                    <div className={ styles.date }>2018-07-12</div>
+                                </div>
+                                <p className={ styles.place }>@ Purgeraj, Zagreb</p>
+                                <p>Venenatis commodo non suscipit taciti mi adipiscing dictum ullamcorper faucibus a euismod vestibulum scelerisque a a aliquam faucibus ac nunc bibendum hac torquent quam himenaeos habitant ac eros tristique. Amet hendrerit leo orci elit a ridiculus suspendisse sociosqu dapibus sapien euismod at porttitor ut lorem sodales et rhoncus. A ac.</p>
+                                <div className={ styles.articleFooter }>
+                                    <button>More details</button>
+                                </div>
+                            </div>
+                        </article>
+                        <article>
+                            <div className={ styles.articleHeaderImg }>
+                                <img src={ tvShowImage } alt="Project header" />
+                            </div>
+                        </article>
+                        <article>
+                            <div className={ styles.articleHeaderImg }>
+                                <img src={ specialImage } alt="Project header" />
+                            </div>
+                        </article>
+                        <article>
+                            <div className={ styles.articleHeaderImg }>
+                                <img src={ tvShowImage } alt="Project header" />
+                            </div>
+                        </article>
+                    </section>
+                </main>
+
             </React.Fragment>
         );
     }
