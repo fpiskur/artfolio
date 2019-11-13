ee4e4d5b7ca97291b9dbda3854c4c3e389e66287
chof09 <filip.piskur@gmail.com>
2019-11-13 13:07:38 +0100

    Finished ProfilePage design

--- src/containers/ProfilePage/ProfilePage.js
+++ src/containers/ProfilePage/ProfilePage.js
@@ -25,10 +25,7 @@ class ProfilePage extends Component {
                     </div>
                     <div className={ styles.info }>
                         <div className={ styles.availability }>
-                            <select>
-                                <option>Available</option>
-                                <option>Not available</option>
-                            </select>
+                            Available
                         </div>
                         <div className={ styles.bio }>
                             <h2>Jim Jimson</h2>
@@ -46,7 +43,9 @@ class ProfilePage extends Component {
                         <button>Projects</button>
                         <button>About me</button>
                     </div>
-                    <section className={ styles.projects }>
+
+                    {/* Projects component */}
+                    {/* <section className={ styles.projects }>
                         <article>
                             <div className={ styles.articleHeaderImg }>
                                 <img src={ specialImage } alt="Project header" />
@@ -57,27 +56,89 @@ class ProfilePage extends Component {
                                     <div className={ styles.date }>2018-07-12</div>
                                 </div>
                                 <p className={ styles.place }>@ Purgeraj, Zagreb</p>
-                                <p>Venenatis commodo non suscipit taciti mi adipiscing dictum ullamcorper faucibus a euismod vestibulum scelerisque a a aliquam faucibus ac nunc bibendum hac torquent quam himenaeos habitant ac eros tristique. Amet hendrerit leo orci elit a ridiculus suspendisse sociosqu dapibus sapien euismod at porttitor ut lorem sodales et rhoncus. A ac.</p>
+                                <p>Lorem ipsum dolor sit amet consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma.</p>
                                 <div className={ styles.articleFooter }>
-                                    <button>More details</button>
+                                    <button className={ styles.moreDetailsBtn }>More details</button>
                                 </div>
                             </div>
                         </article>
-                        <article>
-                            <div className={ styles.articleHeaderImg }>
-                                <img src={ tvShowImage } alt="Project header" />
+                    </section> */}
+                    
+                    {/* About me component */}
+                    <section className={ styles.aboutMe }>
+                        <div className={ styles.aboutLong }>
+                            <h3>About me</h3>
+                            <p>Ut nam pulvinar nam condimentum pulvinar consectetur nisi adipiscing cum rutrum vestibulum ridiculus neque amet habitant ullamcorper at consectetur consequat nibh. Vitae adipiscing mus at volutpat a at condimentum id vestibulum vitae odio hac congue ornare eu libero semper a est dictumst molestie sapien. Eu a fringilla cubilia vulputate torquent eu id a hac non iaculis magna praesent parturient suspendisse fusce a. Eu lacinia at at himenaeos ipsum praesent enim magna elit suscipit ac scelerisque torquent parturient magna mi nulla consequat ullamcorper felis class magnis blandit a mus sem eleifend. Euismod a scelerisque duis mi himenaeos est ad vestibulum felis tempor metus suspendisse a adipiscing sed at hendrerit sit vivamus ullamcorper vestibulum consectetur natoque.</p>
+                        </div>
+                        <div className={ styles.skills }>
+                            <h4>DANCING SKILLS:</h4>
+                            <p>
+                                <span>waltz</span>
+                                <span>tango</span>
+                                <span>tap dancing</span>
+                            </p>
+                            <h4>MUSICAL SKILLS:</h4>
+                            <p>
+                                <span>guitar</span>
+                                <span>trombone</span>
+                            </p>
+                            <h4>ACTING SKILLS:</h4>
+                            <p>
+                                <span>comedy</span>
+                                <span>drama</span>
+                            </p>
+                        </div>
+                        <div className={ styles.list }>
+                            <h3>Education</h3>
+                            <div className={ styles.listItem }>
+                                <div className={ styles.listInfo }>
+                                    <h4>Whatever Academy</h4>
+                                    <p>my profession</p>
+                                </div>
+                                <div className={ styles.listDate }>
+                                    1999. - 2003.
+                                </div>
                             </div>
-                        </article>
-                        <article>
-                            <div className={ styles.articleHeaderImg }>
-                                <img src={ specialImage } alt="Project header" />
+                            <div className={ styles.listItem }>
+                                <div className={ styles.listInfo }>
+                                    <h4>Whatever Highschool</h4>
+                                    <p>my title</p>
+                                </div>
+                                <div className={ styles.listDate }>
+                                    1995. - 1999.
+                                </div>
                             </div>
-                        </article>
-                        <article>
-                            <div className={ styles.articleHeaderImg }>
-                                <img src={ tvShowImage } alt="Project header" />
+                            <div className={ styles.listItem }>
+                                <div className={ styles.listInfo }>
+                                    <h4>Whatever Elementary</h4>
+                                    <p>something diploma</p>
+                                </div>
+                                <div className={ styles.listDate }>
+                                    1987. - 1995.
+                                </div>
                             </div>
-                        </article>
+                        </div>
+                        <div className={ styles.list }>
+                            <h3>Workshops</h3>
+                            <div className={ styles.listItem }>
+                                <div className={ styles.listInfo }>
+                                    <h4>Burek workshop</h4>
+                                    <p>with Pero Perić</p>
+                                </div>
+                                <div className={ styles.listDate }>
+                                    September 2004.
+                                </div>
+                            </div>
+                            <div className={ styles.listItem }>
+                                <div className={ styles.listInfo }>
+                                    <h4>Sarma workshop</h4>
+                                    <p>with Đurđa Adlešić</p>
+                                </div>
+                                <div className={ styles.listDate }>
+                                    January 2005.
+                                </div>
+                            </div>
+                        </div>
                     </section>
                 </main>
 
